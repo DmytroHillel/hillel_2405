dca68fdeeed7f7b91c9eb312cc9882dd15f75a45
Dmytro Havrylenko <havrilenko.dmitry@gmail.com>
2024-06-09 22:41:43 +0300

    hw_5_2 was changed for rewiew

--- hw5_2_for_lesson_5/homework_5_2.py
+++ hw5_2_for_lesson_5/homework_5_2.py
@@ -4,50 +4,47 @@
 # 3 - check that all people in modified list with records indexes 6, 10, 13
 #   have age >=30. Print condition check result
 import random
+
 people_records = [
-  ('John', 'Doe', 28, 'Engineer', 'New York'),
-  ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
-  ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
-  ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
-  ('Michael', 'Brown', 22, 'Student', 'Seattle'),
-  ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
-  ('David', 'Miller', 33, 'Software Developer', 'Austin'),
-  ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
-  ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
-  ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
-  ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
-  ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
-  ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
-  ('Ava', 'White', 42, 'Journalist', 'San Diego'),
-  ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
+    ('John', 'Doe', 28, 'Engineer', 'New York'),
+    ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
+    ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
+    ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
+    ('Michael', 'Brown', 22, 'Student', 'Seattle'),
+    ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
+    ('David', 'Miller', 33, 'Software Developer', 'Austin'),
+    ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
+    ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
+    ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
+    ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
+    ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
+    ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
+    ('Ava', 'White', 42, 'Journalist', 'San Diego'),
+    ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
 ]
 
 separator = '================='
 
-#1 added new person
+# 1 added new person
 people_records.insert(0, ('Den', 'Neith', 25, 'Seller', 'Oklahoma'))
 print(people_records)
 print(separator)
 
-#2 replace indexes in list
-fifth = people_records[5]
-first = people_records[1]
-people_records[5] = first
-people_records[1]= fifth
+# 2 replace indexes in list
+people_records[1], people_records[5] = people_records[5], people_records[1]
 print(people_records)
 print(separator)
 
-#3 people age
-var = True
+# 3 people age
 first_person = people_records[6]
 second_person = people_records[10]
 third_person = people_records[13]
 people_list = [first_person, second_person, third_person]
 for person in people_list:
-  if person[2] >= 30:
-    print(var)
-  else:
-    var = False
-    print(var)
+    if person[2] >= 30:
+        print(False)
+        break
+    else:
+        print(True)
 
 

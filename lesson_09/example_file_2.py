31912c0cd3bc805c6b63aa982e812029af97c1fa
Dmytro Havrylenko <havrilenko.dmitry@gmail.com>
2024-06-22 13:55:09 +0300

    files from 9-th lesson were added, and renemed 6-th lesson folder

---
+++ lesson_09/example_file_2.py
@@ -0,0 +1,10 @@
+from lesson_09.example_file_1 import say_hello  # візьми функцію say_hello із файла tests.example_file_1
+
+
+def greeting(name):
+    print(f'Greetings {name}')
+
+
+if __name__ == '__main__':
+    greeting('Den')
+    say_hello('Ivan')

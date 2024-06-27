467fe822f5707143f4b0b49f8f2e07b71c83fc61
DmytroHillel <dmytrohavrylenko7@gmail.com>
2024-06-27 21:04:49 +0300

    Homework05 2 (#5)
    * clear from all files
    
    * added changes in hw_for_lesson_4 file
    
    * file for hw is added, hw is done and ready for check
    
    * create file for hw_5_2 and added hw for rewiew
    
    * hw_5_2 was changed for rewiew
    
    ---------
    
    Co-authored-by: Dmytro Havrylenko <havrilenko.dmitry@gmail.com>

---
+++ hw5_2_for_lesson_5/homework_5_2.py
@@ -0,0 +1,50 @@
+# Given list of tuples (name, surname, age, profession, City location)
+# 1 - Add your new record o the beginning of the given list
+# 2 - In modified list swap elements with indexes 1 and 5 (1<->5). Print result
+# 3 - check that all people in modified list with records indexes 6, 10, 13
+#   have age >=30. Print condition check result
+import random
+
+people_records = [
+    ('John', 'Doe', 28, 'Engineer', 'New York'),
+    ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
+    ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
+    ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
+    ('Michael', 'Brown', 22, 'Student', 'Seattle'),
+    ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
+    ('David', 'Miller', 33, 'Software Developer', 'Austin'),
+    ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
+    ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
+    ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
+    ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
+    ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
+    ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
+    ('Ava', 'White', 42, 'Journalist', 'San Diego'),
+    ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
+]
+
+separator = '================='
+
+# 1 added new person
+people_records.insert(0, ('Den', 'Neith', 25, 'Seller', 'Oklahoma'))
+print(people_records)
+print(separator)
+
+# 2 replace indexes in list
+people_records[1], people_records[5] = people_records[5], people_records[1]
+print(people_records)
+print(separator)
+
+# 3 people age
+first_person = people_records[6]
+second_person = people_records[10]
+third_person = people_records[13]
+people_list = [first_person, second_person, third_person]
+for person in people_list:
+    if person[2] >= 30:
+        print(False)
+        break
+    else:
+        print(True)
+
+

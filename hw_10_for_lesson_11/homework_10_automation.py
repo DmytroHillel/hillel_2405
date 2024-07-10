7395c597e29cd00406ca2115121977322aa52fe4
DmytroHillel <dmytrohavrylenko7@gmail.com>
2024-07-10 19:18:57 +0300

    hw file for 10-th lesson was added for teachers rewiew (#13)
    * hw file for 10-th lesson was added for teachers rewiew
    
    * commit for checkout
    
    * changed hw for second teachers rewiew
    
    ---------
    
    Co-authored-by: Dmytro Havrylenko <havrilenko.dmitry@gmail.com>

---
+++ hw_10_for_lesson_11/homework_10_automation.py
@@ -0,0 +1,90 @@
+"""Ваша команда та ви розробляєте систему входу для веб-додатка,
+і вам потрібно реалізувати тести на функцію для логування подій в системі входу.
+Дано функцію, напишіть набір тестів для неї.
+"""
+
+import logging
+import pytest
+
+
+def log_event(username: str, status: str):
+    """
+    Логує подію входу в систему.
+
+    username: Ім'я користувача, яке входить в систему.
+
+    status: Статус події входу:
+
+    * success - успішний, логується на рівні інфо
+    * expired - пароль застаріває і його слід замінити, логується на рівні warning
+    * failed  - пароль невірний, логується на рівні error
+    """
+    log_message = f"Login event - Username: {username}, Status: {status}"
+
+    # Створення та налаштування логера
+    logging.basicConfig(
+        filename='login_system.log',
+        level=logging.INFO,
+        format='%(asctime)s - %(message)s - %(levelname)s'  #,
+        # force=True
+    )
+    logger = logging.getLogger("log_event")
+
+    # Логування події
+    if status == "success":
+        logger.info(log_message)
+    elif status == "expired":
+        logger.warning(log_message)
+    else:
+        logger.error(log_message)
+
+
+# @pytest.mark.parametrize('status', ['success'])
+# def test_log_param_success(status):
+#     log_event('John', status)
+
+
+# def test_log_success():
+#     assert log_event('John', 'success')
+#
+#
+# def test_log_expired():
+#     assert log_event('Natalka', 'expired')
+#
+#
+# def test_log_error():
+#     assert AssertionError
+#     log_event('Vasyl', 'glvld')
+#
+
+@pytest.mark.parametrize('name, status, expected', [("John", "success", "Login event - "
+                                                                        "Username: John, Status: success"),
+                                                    ("Lisa", "expired", "Login event - "
+                                                                        "Username: Lisa, Status: expired"),
+                                                    ("Joh", "succ", "Login event - Username: John, Status: success")],
+                                                    ids=['tes_1', 'test_2', 'test_3'])
+def test_success_log_2(name, status, expected):
+    log_event(name, status)
+    with open("login_system.log", "r") as f:
+        log_rows = f.readlines()
+        assert expected in log_rows[-1]
+
+    # with open("login_system.log", "r") as f:
+    #     assert f"Login event - Username: {param_1}, Status: {param_2}" == expected in f.read()
+
+# def test_success_log_2():
+#     log_event("John", "success")
+#     with open("login_system.log", "r") as f:
+#         assert "Login event - Username: John, Status: success" in f.read()
+#
+#
+# def test_expired_log():
+#     log_event("John", "expired")
+#     with open("login_system.log", "r") as f:
+#         assert "Login event - Username: John, Status: expired" in f.read()
+#
+#
+# def test_failed_log():
+#     log_event("John", "failed")
+#     with open("login_system.log", "r") as f:
+#         assert "Login event - Username: John, Status: failed" in f.read()

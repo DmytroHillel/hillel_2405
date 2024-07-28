290b8a28a31a0555bd9b959dd155ca25f2c53809
Dmytro Havrylenko <havrilenko.dmitry@gmail.com>
2024-07-28 22:56:10 +0300

    files for lesson 14, 15, 16, 17, 18 were added

---
+++ lesson_16/abstract.py
@@ -0,0 +1,39 @@
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+
+    @abstractmethod
+    def say_smth(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def walk(self):
+        raise NotImplementedError
+
+    def say_hi(self):
+        print('Hi')
+
+    def Animal_say_hi(self):
+        print('Hi')
+
+
+class Cat(Animal):
+
+    def say_smth(self):
+        print('Mew')
+
+    def walk(self):
+        print('Mew')
+
+    # @abstractmethod
+    # def walk(self):
+    #     raise NotImplementedError
+
+
+if __name__ == '__main__':
+    cat = Cat()
+
+    cat.say_smth()
+    cat.say_hi()
